07791c22056a469197bf33cdbd80813e78ac3b3e
bakali1 <bakalimohammed43@gamil.com>
2026-03-02 14:26:20 +0000

    fix flake errors

--- app/user_manager.py
+++ app/user_manager.py
@@ -1,18 +1,26 @@
 class UserManager:
+
     def __init__(self):
-        self.users=[]
-    def add_user(self,username):
+        self.users = []
+
+    def add_user(self, username):
         if not username:
             raise ValueError("le nom d'utilisateur est obligatoire")
+
         if username in self.users:
             raise ValueError("Utilisateur deja existant")
+
         self.users.append(username)
-    def remove(self,username):
+
+    def remove_user(self, username):
         if username not in self.users:
             raise ValueError("Utilisateur introuvable")
+
         self.users.remove(username)
+
     def count_users(self):
         return len(self.users)
+
 
 def count_users(users):
     return len(users)
